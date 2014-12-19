7169f61d49da7660a4db00bdd78a7b353d964020
Patrick Landin <patricklandin@gmail.com>
2014-12-19 15:16:33 -0800

    first commit. mapkit view present.

---
+++ Fancy Places/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fancy Places
+//
+//  Created by Patrick Landin on 12/19/14.
+//  Copyright (c) 2014 Patrick Landin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
